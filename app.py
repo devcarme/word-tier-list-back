bcbb331cfe0245f9b3378e1445b3bbb6ac4d3104
devcarme <ducarmeloick@gmail.com>
2022-03-07 17:12:04 +0100

    Initial Commit

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import json
+import os
 
 import pymongo as pymongo
 from bson import ObjectId
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-client = pymongo.MongoClient(
-    "mongodb+srv://admin:z6HFhyO952qVJvAT@cluster0.ay330.mongodb.net/word_tier_list?retryWrites=true&w=majority")
+client = pymongo.MongoClient(os.environ.get("DATABASE_URI"))
 db = client.words
 
 class JSONEncoder(json.JSONEncoder):
@@ -17,6 +20,7 @@ class JSONEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, o)
 
 @app.route('/')
+@cross_origin()
 def hello_world():  # put application's code here
     return 'Hello World!'
 
@@ -50,18 +54,29 @@ def get_word_by_id(id):
 def delete_word_by_id(id):
     # delete the word from the database
     db.words.delete_one({'_id': id})
-    return 'Word deleted successfully'
+    response = app.response_class(
+        response=json.dumps({'message': 'Word deleted successfully'}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 def add_word():
     # get the data from the request
     data = request.get_json()
     # insert the data into the database
     db.words.insert_one(data)
-    return 'Word added successfully'
+    data = JSONEncoder().encode(data)
+    response = app.response_class(
+        response=json.dumps({'message': 'Word added successfully', 'data': data}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 def get_all_words():
-    # get all the words from the database
-    words = db.words.find()
+    # get all the words from the database sorted by tier
+    words = db.words.find().sort('tier', pymongo.DESCENDING)
     # convert the words to a list
     words_list = [word for word in words]
     words_list = JSONEncoder().encode(words_list)
@@ -73,17 +88,30 @@ def delete_word():
     data = request.get_json()
     # delete the word from the database
     db.words.delete_one(data)
-    return 'Word deleted successfully'
+    data = JSONEncoder().encode(data)
+    response = app.response_class(
+        response=json.dumps({'message': 'Word deleted successfully', 'data': data}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 def update_word():
     # get the data from the request
     data = request.get_json()
+    # data = JSONEncoder().encode(data)
+    word = data['word']
     # update the word in the database
-    data['_id'] = ObjectId(data['_id'])
-    query = {"_id": data['_id']}
-    new_values = {"$set": {'_id': data['_id'], 'word': data['word'], 'tier': data['tier'], 'definition': data['definition']}}
+    query = {"_id": ObjectId(word['_id'])}
+    new_values = {"$set": {'name': word['name'], 'tier': word['tier'], 'definition': word['definition']}}
     db.words.update_one(query, new_values)
-    return 'Word updated successfully'
+    response = app.response_class(
+        response=json.dumps({'message': 'Word updated successfully', 'data': data}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 if __name__ == '__main__':
     app.run()
+
